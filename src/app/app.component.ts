a848b90876918cfb1e7d9f98f4f6d231628c03e4
jintian <yjt@awt-system.com.tw>
2025-09-11 17:59:01 +0800

    修正调用 dll 的方法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { uxp } from "@adobe/cc-ext-uxp-types";
+import uxp from 'uxp';
 
 @Component({
   standalone: false,
@@ -6,11 +8,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'photoshop-uxp-plugin';
+
+  callback = (o) => {
+    console.log("Message from " + o.pluginId + ":" + o.message);
+  }
+
+  ngOnInit(): void {
+    window.require('photoshop').messaging.addSDKMessagingListener(this.callback);
+    window.require('photoshop').messaging.removeSDKMessagingListener(this.callback);
+  }
 
   doClick = (evt:any) => {
 
-    console.log(evt);
+    // console.log(evt);
+
+    // console.log(photoshop);
+    console.log("APP:WINDOW_PHOTOSHOP::", window?.require('photoshop'));
+    // console.log(uxp);
+
+    let messageContent = {
+        "Publisher__AfterCloseDoc": {
+          "docCount": 3
+        }
+    };
+    const payload = {Msge: JSON.stringify(messageContent)}
+    window.require('photoshop').messaging.sendSDKPluginMessage("awt.xms.photoshop", payload);
+
   }
 }
